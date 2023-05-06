dcc7c08316e4fc6dc7c7e2d3c3c910af0b4436c0
Vane <keyliesanchez19@gmail.com>
2023-05-06 11:06:55 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio4/Clase/Alumno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            IPersona persona = new Nombre("Juan", 30);
+            persona.Saludar();
+            persona.CumplirAños();
+            Console.WriteLine("La edad de la persona es: " + persona.Edad);
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Ejercicio4/Interface/IPersona.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4.Interface
+{
+    {
+    public interface IPersona
+    {
+        string Nombre { get; }
+        int Edad { get; }
+        void Saludar();
+        void CumplirAños();
+    }
+
+    public class Persona : IPersona
+    {
+        private string _nombre;
+        private int _edad;
+
+        public Persona(string nombre, int edad)
+        {
+            _nombre = nombre;
+            _edad = edad;
+        }
+
+        public string Nombre
+        {
+            get { return _nombre; }
+        }
+
+        public int Edad
+        {
+            get { return _edad; }
+        }
+
+        public void Saludar()
+        {
+            Console.WriteLine("Hola, mi nombre es " + _nombre);
+        }
+
+        public void CumplirAños()
+        {
+            _edad++;
+        }
+    }
+}
